e55cb7316653afacbbce788422d9b92ac462aa3e
lipan <lipan.me@qq.com>
2020-10-16 17:30:49 +0800

    阻塞队列

---
+++ src/main/java/com/panpan/walle/atguigu/BlockingQueueDemo.java
@@ -0,0 +1,15 @@
+package com.panpan.walle.atguigu;
+
+import java.util.List;
+
+/**
+ * 1,队列
+ * 2，阻塞队列
+ *  2.1 阻塞队列有没有好的没有
+ *  2.2 不得不阻塞，你如何管理
+ */
+public class BlockingQueueDemo {
+    public static void main(String[] args) {
+        List list = null;
+    }
+}
